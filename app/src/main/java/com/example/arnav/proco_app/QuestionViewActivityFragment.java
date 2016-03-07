321216f8fbae9cda71fe8ae2f08f61c61bc6f097
Arnav Agarwal <agarwl13@illinois.edu>
2016-03-06 18:43:10 -0600

    Initial commit(basic android project)

---
+++ app/src/main/java/com/example/arnav/proco_app/QuestionViewActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.arnav.proco_app;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class QuestionViewActivityFragment extends Fragment {
+
+    public QuestionViewActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_question_view, container, false);
+    }
+}
